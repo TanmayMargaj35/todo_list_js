9c2700fd77a9a6622778ae38dcf1c45ab44ffeba
Tanmay Margaj <tanmaymargaj35@gmail.com>
2025-09-14 11:59:05 +0530

    Initial Commit for the todo list structure

---
+++ script.js
@@ -0,0 +1,7 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+function saveTasks() {
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+// Initial render 
+renderTasks();
